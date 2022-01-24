ea0786b6f0d4ea3e2ffdaacad8bfa029cba36fe8
gsomer <gasomer75@hotmail.com>
2022-01-23 22:24:16 -0800

    added prompts for character selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,11 @@ var randomNumber = function(min, max) {
   return character;
 };
 
-// AVAILABLE CHARACTERS
-
-var lowerValues = "abcdefghijklmnopqrstuvwxyz";
-var upperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberValues = "0123456789";
-var symbolValues = "!@#$%^&*()/\*_";
-
-
-var randomCharacter = {
-  lower: randomLower,
-  upper: randomUpper,
-  digit: randomDigit,
-  symbol: randomSymbol
-}
-
 // START HERE WITH PAGE INTRO
-window.alert("Press 'Generate Password' to get started.")
+window.alert("Press 'Generate Password' to get started.");
 
 // FUNCTION TO START PROMPTS AND GENERATE PASSWORD
-function generatePassword(characterChoices, passwordLength) {
+function generatePassword() {
   
   // PASSWORD LENGTH FUNCTION
   var lengthText = window.prompt("Choose a password length from 8 to 128 characters.");
@@ -36,71 +21,51 @@ function generatePassword(characterChoices, passwordLength) {
     } else {
       window.alert("You have chosen a password length of " + passwordLength + " characters.");
     }
+  console.log(passwordLength);
 
-    
+  // FUNCTIONS TO DETERMINE PASSWORD CHARACTER CRITERIA
+  var characterChoices = function() {
+    var lowercase = window.confirm("To include lower case letters in your password click 'OK', otherwise click 'CANCEL'.");
+      if (!lowercase) {
+        window.alert("You WILL NOT have lower case letters in your password");
+      } else {
+        window.alert("You WILL have lower case letters in your password.");
+        
+      }
+      console.log(lowercase);
 
-//     var lowercase = window.confirm("To include lower case letters in your password click 'OK', otherwise click 'CANCEL'.");
-    
-//       if (!lowercase) {
-//         window.alert("You WILL NOT have lower case letters in your password");
+      var uppercase = window.confirm("To include UPPER case letters in your password click 'OK', otherwise click 'CANCEL'.");
+      if (!uppercase) {
+        window.alert("You WILL NOT have UPPER case letters in your password");
+      } else {
+        window.alert("You WILL have UPPER case letters in your password.");
         
-//       } else {
-//         window.alert("You WILL have lower case letters in your password.")
+      }
+      console.log(uppercase);
+
+      var numbers = window.confirm("To include numbers in your password click 'OK', otherwise click 'CANCEL'.");
+      if (!numbers) {
+        window.alert("You WILL NOT have numbers in your password");
+      } else {
+        window.alert("You WILL have numbers in your password.");
         
-//         console.log(lowercase);
-//       }
+      }
+      console.log(numbers);
+
+      var symbols = window.confirm("To include special symbols in your password click 'OK', otherwise click 'CANCEL'.");
+      if (!symbols) {
+        window.alert("You WILL NOT have special symbols in your password");
+      } else {
+        window.alert("You WILL have special symbols in your password.");
+        
+      }
+      console.log(symbols);
+  }
+  characterChoices();
+
+  // FUNCTION TO GENERATE NEW PASSWORD BASED ON SELECTED CRITERIA
   
-//     var uppercase = window.confirm("To include UPPER case letters in your password click 'OK', otherwise click 'CANCEL'.");
-    
-//       if (!uppercase) {
-//         window.alert("You WILL NOT have UPPER case letters in your password");
-        
-//       } else {
-//         window.alert("You WILL have UPPER case letters in your password.")
-        
-//         console.log(uppercase);
-//       }
-  
-//     var numbers = window.confirm("To include numbers in your password click 'OK', otherwise click 'CANCEL'.");
-    
-//       if (!numbers) {
-//         window.alert("You WILL NOT have numbers in your password");
-        
-//       } else {
-//         window.alert("You WILL have numbers in your password.")
-        
-//         console.log(numbers);
-//       }
-  
-//     var symbols = window.confirm("To include symbols in your password click 'OK', otherwise click 'CANCEL'.");
-    
-//       if (!symbols) {
-//         window.alert("You WILL NOT have symbols in your password");
-        
-//       } else {
-//         window.alert("You WILL have symbols in your password.")
-        
-//         console.log(symbols);
-//       }
-      
-      
 };
-
-var randomLower = function() {
-  return lowerValues(randomNumber(0, 25));
-}
-
-var randomUpper = function() {
-  return upperValues(randomNumber(0, 25));
-}
-
-var randomDigit = function() {
-  return numberValues(randomNumber(0, 9));
-}
-
-var randomSymbol = function() {
-  return symbolValues(randomNumber(0, 13));
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
